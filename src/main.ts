eb3af4655129acc9f890575181eda7084fcb731a
Nirosha <niroshavennapusa@gmail.com>
2026-03-04 18:38:07 +0530

    caption

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,11 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',          // Allow requests from any origin
+    methods: '*',         // Allow all HTTP methods (GET, POST, etc.)
+    allowedHeaders: '*'   // Allow all headers
+  });
   const port = process.env.PORT || 3000;
   await app.listen(port);
   console.log(`Server is running on port ${port}`);
